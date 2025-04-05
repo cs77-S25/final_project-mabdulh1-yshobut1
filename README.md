226a4650e1f1457bcbe89dde1f35797a16fc291a
Maryam Abdulhussein <mabdulh1@swarthmore.edu>
2025-04-05 00:35:17 -0400

    transferring files

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# final_project
+## Authors
+Maryam Abdulhussein and Yara Shobut
+
+# SwapSwap
+Welcome to SwatSwap, a student-centered, cash-free bartering platform built for the Swarthmore College community. SwatSwap lets users give, trade, and request items — from clothes and books to furniture and plants — in a way that's sustainable, ethical, and free.
+
+## Features
+SCCS Login Integration: Secure Swarthmore login using SCCS credentials.
+
+Product Listings: Post, edit, and manage items you want to give away, trade, or request.
+
+Flexible Trade Options: Support for 1:1 swaps, multi-item trades, and “free” (giveaway) listings.
+
+Wishlist System: Keep track of items you're hoping to receive in the future.
+
+Messaging: In-platform chat for arranging swaps and asking questions.
+
+Follow & Recommend: Follow friends or trusted traders and get item recommendations.
+
+Search & Filter: Sort by category, condition, and trade type.
+
+Notifications: Get alerted when someone messages you or responds to your item.
+
+## Technology Stack
+
+- Backend: Node.js, Express
+- Database: 
+- Frontend: Next.js, Redux for state management
+- Authentication: SCCS login or EmailJS for contact
+- File Storage: Cloudinary
+- File Handling: Multer
+- Email Handling: EmailJS for contact form
+- Deployment: Vercel
+
+## Getting Started
+
+Follow these steps to get a copy of the project up and running on your local machine!!
+
+Things you need to install run the software:
+- Node.js
+- Git
+
+### Installing
+- Clone the repository
+```
+git clone
+```
+
+- Install dependencies
+```
+npm install
+```
+
+- Set up environment variables: *Rename* .sample.env file to .env in the root directory
+
+- Run the application
+```
+npm run dev
+```
+
+- Open your browser and visit `http://localhost:3000` to view the live website.
---
+++ app/Navbar.js
@@ -0,0 +1,448 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState, useCallback } from "react";
+import Link from "next/link";
+import axios from "axios";
+import { Dropdown, Badge, Popover, Empty } from "antd";
+
+import TimeAgoWrapper from "../fractions/TimeAgoWrapper";
+
+import defaultProfile from "../../../public/defaultProfile.svg";
+import Delete from "@/components/icons/Delete.js";
+
+const handleAxiosError = (error) => {
+	const errorMessage =
+		error.response?.data?.message || "Something went wrong. Try again";
+	console.log(errorMessage);
+};
+
+const NotificationItem = ({ notification }) => {
+	const handleReadToggle = useCallback(async () => {
+		try {
+			await axios.patch(`/api/v1/notification/${notification._id}`, {
+				withCredentials: true,
+			});
+		} catch (error) {
+			handleAxiosError(error);
+		}
+	}, []);
+
+	const handleDelete = useCallback(async () => {
+		try {
+			await axios.delete(`/api/v1/notification/${notification._id}`, {
+				withCredentials: true,
+			});
+		} catch (error) {
+			handleAxiosError(error);
+		}
+	}, []);
+
+	const getLinkHref = () => {
+		switch (notification.notificationType) {
+			case "follow":
+				return `/profile/${notification.followedByUser?._id}`;
+			case "feedback":
+				return `/profile/${notification.user}`;
+			case "transaction":
+				return `/transaction/${notification.transactionId}`;
+			default:
+				return "#";
+		}
+	};
+
+	const getAvatarSrc = () => {
+		switch (notification.notificationType) {
+			case "follow":
+				return notification.followedByUser?.avatar || defaultProfile;
+			case "feedback":
+				return notification.feedbackByUser?.avatar || defaultProfile;
+			case "transaction":
+				return notification.transactionUser?.avatar || defaultProfile;
+			default:
+				return defaultProfile;
+		}
+	};
+
+	return (
+		<section className="flex gap-2 border-b">
+			<Link href={getLinkHref()} className="self-center">
+				<div className="relative w-12 h-12">
+					<Image
+						src={getAvatarSrc()}
+						alt="User Avatar"
+						className="rounded-full border-2 border-black object-cover"
+						layout="fill"
+						objectFit="cover"
+					/>
+				</div>
+			</Link>
+			<section className="p-2 flex flex-col w-full">
+				<div className="flex gap-2">
+					<div className="flex flex-col gap-2">
+						<div
+							className={`${
+								notification.isRead
+									? "font-normal"
+									: "font-bold"
+							}`}
+						>
+							{notification.content}
+						</div>
+						<button
+							className="text-xs w-fit"
+							onClick={handleReadToggle}
+						>
+							{notification.isRead ? (
+								<div className="flex items-center">
+									<Image
+										src="/icons/check.svg"
+										alt="Icon to mark notification as unread"
+										width={20}
+										height={20}
+									/>
+									Mark as unread
+								</div>
+							) : (
+								<div className="flex gap-1">
+									<Image
+										src="/icons/doubleCheck.svg"
+										alt="Icon to mark notification as read"
+										width={15}
+										height={15}
+									/>
+									Mark as read
+								</div>
+							)}
+						</button>
+					</div>
+					<Delete
+						className="cursor-pointer w-5 h-5 ml-auto"
+						onClick={handleDelete}
+					/>
+				</div>
+				<TimeAgoWrapper
+					date={new Date(notification.createdAt)}
+					className="mt-1 self-end text-xs"
+				/>
+			</section>
+		</section>
+		
+	);
+};
+
+const Notifications = () => {
+	const [notifications, setNotifications] = useState(null);
+
+	useEffect(() => {
+		const fetchNotifications = async () => {
+			try {
+				const getNotification = await axios.get(
+					"/api/v1/notification/",
+					{
+						withCredentials: true,
+					}
+				);
+				setNotifications(getNotification?.data?.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchNotifications();
+	}, []);
+
+	const hasUnreadNotifications = notifications?.some(
+		(notification) => !notification.isRead
+	);
+
+	const content = (
+		<div className="h-fit max-h-60 overflow-y-auto">
+			{notifications?.length > 0 ? (
+				notifications.map((notification) => (
+					<NotificationItem
+						notification={notification}
+						key={notification._id}
+					/>
+				))
+			) : (
+				<Empty description={<p>You&apos;re all caught up.</p>} />
+			)}
+		</div>
+	);
+
+	return (
+		<Badge dot={hasUnreadNotifications} size="small">
+			<Popover
+				content={content}
+				title="Notifications"
+				trigger="click"
+				arrow={false}
+			>
+				<Image
+					src="/icons/notification.svg"
+					alt="notification icon"
+					width={25}
+					height={25}
+				/>
+			</Popover>
+		</Badge>
+	);
+};
+
+const Navbar = () => {
+	const [userDetails, setUserDetails] = useState(defaultProfile);
+	const [showMobileNav, setShowMobileNav] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [loading, setLoading] = useState(true);
+
+	const toggleMobileNav = () => {
+		setShowMobileNav(!showMobileNav);
+	};
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const user = await axios.get("/api/v1/users/", {
+					withCredentials: true,
+				});
+				if (user?.data?.data) {
+					setIsLoggedIn(true);
+					const userDetails = user.data.data;
+					setUserDetails(userDetails);
+				}
+			} catch (error) {
+				console.log(error);
+				setIsLoggedIn(false);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchUser();
+	}, []);
+
+	const handleLogout = async () => {
+		try {
+			await axios.post("/api/v1/users/logout");
+			setIsLoggedIn(false);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	const items = [
+		{
+			key: "1",
+			label: (
+				<Link href="/transaction" className="p-2">
+					View Transactions
+				</Link>
+			),
+		},
+		{
+			key: "2",
+			label: (
+				<Link className="p-2" href={`/profile/${userDetails?._id}`}>
+					Profile
+				</Link>
+			),
+		},
+		{
+			key: "3",
+			label: (
+				<Link className="p-2" href="/contact">
+					Contact Us
+				</Link>
+			),
+		},
+		{
+			key: "4",
+			label: (
+				<Link className="p-2" href="/login" onClick={handleLogout}>
+					Logout
+				</Link>
+			),
+		},
+	];
+
+	if (loading) {
+		return (
+			<div className="bg-maroon-900 text-white">
+				<div className="mx-auto flex justify-between px-5 py-2 items-center">
+					<Link href="/">
+						<Image
+							src="/logo.png"
+							alt="logo"
+							width={100}
+							height={100}
+						/>
+					</Link>
+					<div className="gap-6 items-center hidden sm:flex">
+						<div className="bg-neutral-400/50 w-20 h-4 animate-pulse rounded-md"></div>
+						<div className="bg-neutral-400/50 w-20 h-4 animate-pulse rounded-md"></div>
+						<div className="bg-neutral-400/50 w-10 h-10 animate-pulse rounded-full"></div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<>
+			<div className="bg-maroon-900 text-white">
+				<div className="mx-auto max-w-7xl flex justify-between px-5 py-2 items-center">
+					<Link href="/">
+						<Image
+							src="/logo.png"
+							alt="logo"
+							width={100}
+							height={100}
+						/>
+					</Link>
+					<div>
+						<ul className="gap-6 items-center hidden sm:flex">
+							{isLoggedIn ? (
+								<div className="flex gap-3 items-center">
+									<li className="mt-2 border-b-2 border-maroon-900 hover:border-maroon-400 cursor-pointer">
+										<Notifications />
+									</li>
+									<li className="py-2 border-b-2 border-maroon-900 hover:border-maroon-400">
+										<Link href="/explore">Explore</Link>
+									</li>
+								</div>
+							) : (
+								<>
+									<Link
+										href="/login"
+										className="px-4 py-2 rounded-lg bg-maroon-900 hover:bg-maroon-700 cursor-pointer"
+									>
+										Login
+									</Link>
+									<Link
+										className="px-4 py-2 rounded-lg hover:bg-maroon-700"
+										href="/signup"
+									>
+										Signup
+									</Link>
+								</>
+							)}
+							{isLoggedIn && (
+								<li className="relative">
+									<Dropdown
+										placement="bottom"
+										menu={{
+											items,
+										}}
+									>
+										<Image
+											src={
+												userDetails?.avatar?.url ||
+												defaultProfile
+											}
+											alt="User Avatar"
+											className="w-10 h-10 rounded-full object-cover"
+											width={44}
+											height={44}
+										/>
+									</Dropdown>
+								</li>
+							)}
+						</ul>
+						<Image
+							src="/icons/mobileMenu.svg"
+							alt="Mobile Navigation icon"
+							className="w-7 h-7 flex sm:hidden cursor-pointer"
+							width={28}
+							height={28}
+							onClick={toggleMobileNav}
+						/>
+					</div>
+				</div>
+				{showMobileNav && (
+					<div className="bg-white text-black fixed top-0 bottom-0 left-0 z-[101] w-full sm:w-72 transition-transform duration-300 ease-in-out transform translate-x-0">
+						<div className="flex justify-between items-center px-5 py-3 border-b border-maroon-900">
+							<h3 className="text-lg font-semibold">Menu</h3>
+							<Image
+								src="/icons/closeIcon.svg"
+								alt="Navigation close icon"
+								className="w-6 h-6 cursor-pointer"
+								width={10}
+								height={10}
+								onClick={toggleMobileNav}
+							/>
+						</div>
+						<div className="mobile_nav_links px-5 py-3 ">
+							<ul className="flex flex-col gap-8 items-center">
+								{isLoggedIn ? (
+									<>
+										<li>
+											<Link
+												href="/explore"
+												onClick={toggleMobileNav}
+											>
+												Explore
+											</Link>
+										</li>
+										<li>
+											<Link
+												href="/transaction"
+												onClick={toggleMobileNav}
+											>
+												View Transactions
+											</Link>
+										</li>
+										<li>
+											<Link
+												href={`/profile/${userDetails?._id}`}
+												onClick={toggleMobileNav}
+											>
+												Profile
+											</Link>
+										</li>
+										<li>
+											<Link
+												href="/contact"
+												onClick={toggleMobileNav}
+											>
+												Contact Us
+											</Link>
+										</li>
+										<li>
+											<Link
+												href="/login"
+												onClick={handleLogout}
+											>
+												Logout
+											</Link>
+										</li>
+									</>
+								) : (
+									<>
+										<li>
+											<Link
+												href="/login"
+												onClick={toggleMobileNav}
+											>
+												Login
+											</Link>
+										</li>
+										<Link
+											href="/signup"
+											onClick={toggleMobileNav}
+										>
+											<button className="bg-maroon-900 text-white px-5 py-2 rounded-lg">
+												Signup
+											</button>
+										</Link>
+									</>
+								)}
+							</ul>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default Navbar;
